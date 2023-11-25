a7fa5bf740658284939795c01a0b3e51d1fcdcdc
Flacype <maxence.vretman@gmail.com>
2023-11-25 16:47:05 +0100

    25/11.2 Ajout de la page CV & responsive

--- src/components/cv/cv.component.ts
+++ src/components/cv/cv.component.ts
@@ -1,4 +1,3 @@
-import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
@@ -6,14 +5,13 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-cv',
   standalone: true,
-  imports: [CommonModule, NgxExtendedPdfViewerModule],
+  imports: [CommonModule],
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.scss'
 })
 export class CvComponent {
 
   constructor(private router: Router) {}
-
 
   navigateTo(target: string) {
     this.router.navigate([target]);
